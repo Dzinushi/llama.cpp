4d63fe2e06e278faf5da82692a2db8548c62d4c9
Dzinushi <dzinushi.kun@gmail.com>
2024-05-07 16:00:37 +0300

    feat: added server start with default settings

---
+++ run_server.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# Source of server configs: https://github.com/ggerganov/llama.cpp/blob/master/examples/server/README.md
+
+# Server variables
+host=127.0.0.1
+port=5002
+
+# Rarely modified variables
+threads=6 # num cpu threads for processing
+main_gpu=0 # gpu number for processing
+
+# Model process variables
+model=./models/Hermes-2-Pro-Mistral-7B.Q6_K.gguf
+n_gpu_layers=51 # (51 max for current model) you must set the optimal option if you will be replacing the default model
+ctx_size=4096
+batch_size=512
+
+# Model prompt format example:
+
+### Instruction:
+### Input:
+### Response:
+
+./server \
+    -t $threads \
+    -ngl $n_gpu_layers \
+    -m $model \
+    -c $ctx_size \
+    -b $batch_size \
+    -mg $main_gpu \
+    --host $host\
+    --port $port
